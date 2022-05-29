a927b132e2bd0ce861353a343e9ee1138f04467a
Nehil Jain <2761999+nehiljain@users.noreply.github.com>
2022-05-29 18:22:07 +0000

    build(precommit): initializing precommit

--- local_csv_io.py
+++ local_csv_io.py
@@ -1,12 +1,13 @@
 from dagster import IOManager
+import os
 import pandas as pd
 
 
 class LocalCSVIOManager(IOManager):
     def handle_output(self, context, obj):
-        #TODO:need to get name from context
-        obj.to_csv(os.get_env("WORKING_DIR", "/tmp") + '/output.csv', header=True, index=False)
+        # TODO:need to get name from context
+        obj.to_csv(os.get_env("WORKING_DIR", "/tmp") + "/output.csv", header=True, index=False)
 
     def load_input(self, context):
-        #TODO: need to get name from context like default io manager
-        return pd.read_csv(os.get_env("WORKING_DIR", "/tmp") + '/output.csv')
+        # TODO: need to get name from context like default io manager
+        return pd.read_csv(os.get_env("WORKING_DIR", "/tmp") + "/output.csv")
